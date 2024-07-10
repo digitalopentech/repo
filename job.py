068a3482312696f5a15de3314d36432c9239563a
digitalopentech <60403569+digitalopentech@users.noreply.github.com>
2024-07-10 11:15:45 -0300

    Update job.py

--- job.py
+++ job.py
@@ -1,68 +1,44 @@
-import logging
-import subprocess
-import tempfile
+from dede.core import Job
 from concurrent.futures import ThreadPoolExecutor, wait
+import tempfile
+import subprocess
+import logging
+import integrations.s3 as s3_integration
+import services.database as database_service
 
-from services import descriptor_service, proto_buffer_service, schema_service, database_service, state_service
-from integrations import s3 as s3_integration
+class ConvertSegmentsJob(Job):
+    def run(self):
+        # Configurações do logger
+        logger = logging.getLogger(__name__)
+        logger.info("Starting conversion of segments to Parquet")
 
-logger = logging.getLogger(__name__)
+        # Parâmetros de entrada
+        table = self.params.get("table")
+        jar_path = self.params.get("jar_path")
 
-class ConvertSegmentsParquet:
-    def __init__(self, table, jar_path):
-        self.table = table
-        self.jar_path = jar_path
+        # Função para converter segmentos
+        def _convert_segments_parquet(table, jar_path):
+            urls = database_service.get_completed_segments_urls(table)
+            if not urls:
+                logger.info(f"{table}: No segments completed. Stopping...")
+                return
 
-    def get_completed_segments_urls(self):
-        return database_service.get_completed_segments_urls(self.table)
+            with tempfile.TemporaryDirectory() as tmp_segments, \
+                 tempfile.TemporaryDirectory() as tmp_parquets:
+                with ThreadPoolExecutor(max_workers=4) as executor:
+                    future_results = []
+                    for url in urls:
+                        file_path = f"{tmp_segments}/{url.split('/')[-1]}.gz"
+                        future_results.append(executor.submit(s3_integration.download_file_from_s3, table, url, file_path))
+                    wait(future_results)
 
-    def download_from_s3(self, url):
-        with tempfile.TemporaryDirectory() as tmp_segments, tempfile.TemporaryDirectory() as tmp_parquets:
-            future_results = []
-            with ThreadPoolExecutor(max_workers=4) as executor:
-                for url in urls:
-                    file_path = f"{tmp_segments}/{url.split('/')[-1]}.gz"
-                    future_results.append(executor.submit(s3_integration.download_from_s3_path, self.table, url, file_path))
-                wait(future_results)
+                    # Executa comando Java para conversão
+                    logger.info(f"{table}: Converting segments...")
+                    optional_args = "--add-exports=java.base/sun.nio.ch=ALL-UNNAMED --add-exports=java.base/jdk.internal.ref=ALL-UNNAMED"
+                    cmd = f"java {optional_args} -jar {jar_path} -dataDir={tmp_segments} -outputDir={tmp_parquets} -overwrite"
+                    response_msg = subprocess.getstatusoutput(cmd)[1]
+                    if response_msg:
+                        raise Exception(f"{response_msg} with status code {status} on create descriptor.")
+                    logger.info(f"{table}: Segments converted.")
 
-                command = f"java -jar {self.jar_path} -dataDir={tmp_segments} -outputDir={tmp_parquets} -overwrite"
-                status, response_msg = subprocess.getstatusoutput(command)
-                if status != 0:
-                    raise Exception(f"{response_msg} with status code {status} on create descriptor.")
-                logger.info(f"{self.table}: segments converted.")
-
-class CreateTable:
-    def __init__(self, payload):
-        self.payload = payload
-
-    def execute(self):
-        try:
-            state_service.init(self.payload)
-            file = proto_buffer_service.create_from_schema_registry(self.payload.TABLE, self.payload.KAFKA_SCHEMREGISTRY_TOPIC)
-            state_service.save_state(self.payload.TABLE, TypeState.RUNNING, msg=f"{self.payload.TABLE}: Proto file created successfully.")
-
-            descriptor_service.create_from_file(file)
-            state_service.save_state(self.payload.TABLE, TypeState.RUNNING, msg=f"{self.payload.TABLE}: Descriptor file created successfully.")
-
-            ms, tc = schema_service.create_from_file(self.payload.DOMAIN, self.payload.MODULE, self.payload.TABLE, file, self.payload.CUSTOM_SCHEMA_MODEL_FILE)
-            state_service.save_state(self.payload.TABLE, TypeState.RUNNING, msg=f"{self.payload.TABLE}: Schema e table config created successfully.")
-
-            database_service.create_from_configs(self.payload.DOMAIN, self.payload.TABLE, ms, tc)
-            state_service.save_state(self.payload.TABLE, TypeState.SUCCESS, msg=f"{self.payload.TABLE}: All steps executed successfully.")
-        except StateAlreadyRegisteredException as sarex:
-            logger.error(f"{self.payload.TABLE}: {sarex} Exiting...")
-            sys.exit(1)
-        except Exception as ex:
-            print(traceback.format_exc())
-            state_service.save_state(self.payload.TABLE, TypeState.ERROR, msg=f"{self.payload.TABLE}: {ex} Stopping...")
-            sys.exit(1)
-
-def convert_all_tables_segments_parquet(payload):
-    try:
-        tables = database_service.get_all_tables()
-        with ThreadPoolExecutor(max_workers=4) as executor:
-            futures = [executor.submit(ConvertSegmentsParquet(table, payload.JAR_PATH).download_from_s3) for table in tables]
-            wait(futures)
-    except Exception as ex:
-        print(traceback.format_exc())
-        sys.exit(1)
+        _convert_segments_parquet(table, jar_path)
