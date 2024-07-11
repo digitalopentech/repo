58b116c7640c510663c399a479e16789bb9ef013
digitalopentech <60403569+digitalopentech@users.noreply.github.com>
2024-07-11 14:55:49 -0300

    Update Entrada_movimento.sql

--- Entrada_movimento.sql
+++ Entrada_movimento.sql
@@ -23,4 +23,4 @@ LEFT JOIN d2dbanking_core_cc_operacao_bancaria_em_efetivada_old cc
     AND cco.cdAgencia = cc.cdAgencia
     AND CAST(cco.nuConta AS BIGINT) = CAST(cc.nuConta AS BIGINT)
 WHERE cco.flContaMigrada = 'S'
-  AND cco.ingestionTime BETWEEN ToDateTime(ago('PT5M'), 'yyyy-MM-dd HH:mm:ss') AND ToDateTime(ago('P4D'), 'yyyy-MM-dd HH:mm:ss')
+  AND cco.ingestionTime BETWEEN ToDateTime(ago('P4DT5M'), 'yyyy-MM-dd HH:mm:ss') AND ToDateTime(now(), 'yyyy-MM-dd HH:mm:ss')
