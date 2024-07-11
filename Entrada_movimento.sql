fde3d983c73ada34e5c851f4f2fc785ff89d6d71
digitalopentech <60403569+digitalopentech@users.noreply.github.com>
2024-07-11 15:33:35 -0300

    Update Entrada_movimento.sql

--- Entrada_movimento.sql
+++ Entrada_movimento.sql
@@ -1,10 +1,10 @@
 SELECT 
     day(ingestionTime) AS days, 
-    count(1) 
+    COUNT(*) 
 FROM 
     d2dbanking_core_cco_operacao_bancaria_em_efetivada_old 
 WHERE 
-    ingestionTime BETWEEN ToDateTime(now() - INTERVAL '3' DAY - INTERVAL '5' MINUTE, 'yyyy-MM-dd HH:mm:ss') 
-    AND ToDateTime(now(), 'yyyy-MM-dd HH:mm:ss') 
+    ingestionTime >= ToDateTime(DATETRUNC('day', CURRENT_TIMESTAMP) - INTERVAL '4' DAY, 'yyyy-MM-dd HH:mm:ss') 
+    AND ingestionTime < ToDateTime(CURRENT_TIMESTAMP - INTERVAL '5' MINUTE, 'yyyy-MM-dd HH:mm:ss') 
 GROUP BY 
     days
