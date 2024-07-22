7e9ca49c858095ee6666b63f0e70440e0a440bf7
digitalopentech <60403569+digitalopentech@users.noreply.github.com>
2024-07-22 17:09:47 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
-# tests/test_example.py
+# tests/test_simple.py
 
 import pytest
 
-class TestExample:
+class TestSimple:
     def test_assert_true(self):
         assert True
+
+    @pytest.mark.parametrize("value", [True, 1, "non-empty string"])
+    def test_parametrized_assert_true(self, value):
+        assert value
