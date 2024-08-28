cec2c66d98224f5f18a45c6da734a6095157a958
digitalopentech <60403569+digitalopentech@users.noreply.github.com>
2024-08-28 09:40:01 -0300

    Update message_stream_geolocation.py

--- message_stream_geolocation.py
+++ message_stream_geolocation.py
@@ -5,10 +5,10 @@ import json
 producer = KafkaProducer(
     bootstrap_servers=['b-3.kafkauatdatamskcluste.n24vdd.c12.kafka.us-east-1.amazonaws.com:9094'],  # Endereço do broker Kafka
     security_protocol="SSL",
-    ssl_cafile="/etc/secrets/pinot/kafka-msk/keystore.jks",  # Caminho para o arquivo de keystore
-    ssl_certfile="/etc/secrets/pinot/kafka-msk/keystore.jks",  # Caminho para o arquivo de keystore
-    ssl_keyfile="/etc/secrets/pinot/kafka-msk/keystore.jks",  # Caminho para o arquivo de keystore
-    ssl_password="QPdqEbaVt1781n0ra1GpYQ3og",  # Senha do keystore
+    ssl_cafile="/caminho/para/ca.crt",  # Caminho para o certificado da CA
+    ssl_certfile="/caminho/para/tls.crt",  # Caminho para o certificado do cliente
+    ssl_keyfile="/caminho/para/tls.key",  # Caminho para a chave privada do cliente
+    ssl_password=None,  # Caso a chave privada esteja sem senha, use None
     value_serializer=lambda v: json.dumps(v).encode('utf-8')  # Serializador de mensagens JSON
 )
 
